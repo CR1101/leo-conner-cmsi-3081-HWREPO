07c5ef1cf6a8af87f061272ad01be1b4d0bc3d5f
CR1101 <thisistheemailforgit88@gmail.com>
2024-10-06 20:17:41 -0700

    Quaternion

--- java/Exercises.java
+++ java/Exercises.java
@@ -44,9 +44,64 @@ public class Exercises {
             .count();
         }
     }
+
+
 }
 
+record Quaternion(double a, double b, double c, double d) {
+    
+    public List<Double> coefficients() {
+        return List.of(a, b, c, d);
+    }
+    
+    public static final Quaternion ZERO = new Quaternion(0, 0, 0, 0);
+    public static final Quaternion I = new Quaternion(0, 1, 0, 0);
+    public static final Quaternion J = new Quaternion(0, 0, 1, 0);
+    public static final Quaternion K = new Quaternion(0, 0, 0, 1); 
 
-// Write your Quaternion record class here
+    public Quaternion {
+        if (Double.isNaN(a)||Double.isNaN(b)||Double.isNaN(c)||Double.isNaN(d)) {
+            throw new IllegalArgumentException("Coefficients cannot be NaN");
+        }
+        
+    }
+    public Quaternion plus(Quaternion other) {
+        return new Quaternion(
+            this.a + other.a,
+            this.b + other.b,
+            this.c + other.c,
+            this.d + other.d
+        );
+    }
+    public Quaternion conjugate() {
+        return new Quaternion(this.a, -this.b, -this.c, -this.d);
+    }
+
+    public Quaternion times(Quaternion other) {
+        double newA = this.a * other.a - this.b * other.b - this.c * other.c - this.d * other.d;
+        double newB = this.a * other.b + this.b * other.a + this.c * other.d - this.d * other.c;
+        double newC = this.a * other.c - this.b * other.d + this.c * other.a + this.d * other.b;
+        double newD = this.a * other.d + this.b * other.c - this.c * other.b + this.d * other.a;
+        return new Quaternion(newA, newB, newC, newD);
+    }
+    @Override public String toString() {
+        if (this.a == 0 && this.b == 0 && this.c == 0 && this.d == 0) {
+            return "0";
+        }
+
+        String aTemplate = this.a == 0 ? "" : String.valueOf(this.a);
+        String bTemplate = this.b == 0 ? "" : (this.b < 0 ? "-" : (this.a != 0 ? "+" : "")) + 
+                          ((Math.abs(this.b) == 1) ? "" : Math.abs(this.b)) + "i";
+        String cTemplate = this.c == 0 ? "" : (this.c < 0 ? "-" : (this.a != 0 || this.b != 0 ? "+" : "")) + 
+                          ((Math.abs(this.c) == 1) ? "" : Math.abs(this.c)) + "j";
+        String dTemplate = this.d == 0 ? "" : (this.d < 0 ? "-" : (this.a != 0 || this.b != 0 || this.c != 0 ? "+" : "")) + 
+                          ((Math.abs(this.d) == 1) ? "" : Math.abs(this.d)) + "k";
+
+        return aTemplate + bTemplate + cTemplate + dTemplate;
+    } 
+    }
+
+
+
 
 // Write your BinarySearchTree sealed interface and its implementations here
